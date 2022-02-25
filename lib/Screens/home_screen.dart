9cafa50dd5293ec72525b93e357528f16cf90904
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-02-25 14:34:11 +0300

    Init dio

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Dio? dio;
+  var postIDList = <int>[];
+  bool isIDLoading = false;
+  bool isPostsLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    BaseOptions options = BaseOptions();
+    options.baseUrl = 'https://hacker-news.firebaseio.com/';
+    dio = Dio(options);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackernews_read_app/Screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
